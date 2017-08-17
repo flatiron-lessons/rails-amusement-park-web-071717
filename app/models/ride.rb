9ff2efb08f9731bb84d00ea7f1c4af479ac6c729
github username <github email address>
2017-08-17 13:41:33 -0400

    created all models

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,22 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    user = User.find(self.user_id)
+  	attraction = Attraction.find(self.attraction_id)
+
+  	if user.tickets >= attraction.tickets && user.height >= attraction.min_height
+  		user.tickets -= attraction.tickets
+  		user.happiness += 4
+  		user.nausea += 2
+  		user.save
+  	elsif user.height >= attraction.min_height
+  		"Sorry. You do not have enough tickets to ride the #{attraction.name}."
+  	elsif user.tickets >= attraction.tickets
+  		"Sorry. You are not tall enough to ride the #{attraction.name}."
+  	else
+  		"Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+  	end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+  	# returns sad when user is more nauseous than happy
+  	if self.happiness <= self.nausea
+  		"sad"
+ 	  else # returns happy when the user is  more happy than nauseous
+  		"happy"
+  	end
+  end
 end
